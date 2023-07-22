5d82e482530a05bbebf3c7601ca7a2bf1202817a
Utsav Barnwal <u.barnwal36@gmail.com>
2023-07-22 17:11:25 +0700

    use the app directory

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Starter: NextJS with Tailwind in TypeScript",
+  description: "NextJS 13 starter template using app directory",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <main className="p-8">
+      Hello from <a href="https://nextjs.org/">NextJS</a> with{" "}
+      <a href="https://tailwindcss.com/">Tailwind</a> in{" "}
+      <a href="https://typescriptlang.org/">TypeScript</a> starter template!
+    </main>
+  );
+};
+
+export default Home;
